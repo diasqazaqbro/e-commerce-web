8bc82d7d19adef9e569d9c3cb966be621c21eb4f
diasqazaqbro <adilovdiasdog2006@mail.ru>
2023-06-12 19:31:36 +0600

    hope its last correct 2.0

--- pages/cart.js
+++ pages/cart.js
@@ -121,14 +121,12 @@ export default function CartPage() {
 		return (
 			<>
 				<Header />
-				<Center>
-					<ColumnsWrapper>
-						<Box>
-							<h1>Спасибо за покупку</h1>
-							<p>Мы отправили вам на почту информацию.</p>
-						</Box>
-					</ColumnsWrapper>
-				</Center>
+				<ColumnsWrapper>
+					<Box>
+						<h1>Спасибо за покупку</h1>
+						<p>Мы отправили вам на почту информацию.</p>
+					</Box>
+				</ColumnsWrapper>
 			</>
 		)
 	}
